a5632f6233f80ac1ee454cffa526ca094a4f377a
LawrenceHan <yangfei6565@163.com>
2017-07-13 11:51:39 +0800

    Added license.

--- Sources/UIView+TopViewController.swift
+++ Sources/UIView+TopViewController.swift
@@ -1,3 +1,24 @@
+//  MIT License
+//
+//  Copyright (c) 2017 Lawrence
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 //  UIView+TopViewController.swift
 //  DeeplinkNavigator
@@ -10,14 +31,17 @@ import UIKit
 
 extension UIView {
     
-    /// Returns the current view's top most view controller.
-    public var lhw_topMostViewController: UIViewController? {
-        return UIViewController.lhw_topMost(of: parentViewController)
+    /// Returns the current view's top most navigationController.
+    public var lhw_navigationController: UINavigationController? {
+        if let navigationController = lhw_parentViewController?.navigationController {
+            return navigationController
+        }
+        return UIViewController.lhw_topMost(of: lhw_parentViewController)?.navigationController
     }
     
-    /// Find a UIView's parentViewController by 
+    /// Returns current view's parentViewController by
     /// traverse its responder chain
-    var parentViewController: UIViewController? {
+    public var lhw_parentViewController: UIViewController? {
         var parentResponder: UIResponder? = self
         while parentResponder != nil {
             parentResponder = parentResponder!.next
