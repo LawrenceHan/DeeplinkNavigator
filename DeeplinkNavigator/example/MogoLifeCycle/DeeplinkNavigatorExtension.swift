2edb959cd7140b4efcaea574c7c32c1bb91a6145
spf-iOS <spf-iOS@gitHub.com>
2017-07-24 11:54:37 +0800

    123
    pod

---
+++ DeeplinkNavigator/example/MogoLifeCycle/DeeplinkNavigatorExtension.swift
@@ -0,0 +1,113 @@
+//
+//
+//                        _____
+//                       / ___/____  ____  ____ _
+//                       \__ \/ __ \/ __ \/ __ `/
+//                      ___/ / /_/ / / / / /_/ /
+//                     /____/\____/_/ /_/\__, /
+//                                      /____/
+//
+//                .-~~~~~~~~~-._       _.-~~~~~~~~~-.
+//            __.'              ~.   .~              `.__
+//          .'//                  \./                  \\`.
+//        .'//                     |                     \\`.
+//      .'// .-~"""""""~~~~-._     |     _,-~~~~"""""""~-. \\`.
+//    .'//.-"                 `-.  |  .-'                 "-.\\`.
+//  .'//______.============-..   \ | /   ..-============.______\\`.
+//.'______________________________\|/______________________________`.
+//
+//
+//  DeeplinkNavigatorExtension.swift
+//  DeeplinkNavigator
+//
+//  Created by song on 2017/7/18.
+//  Copyright © 2017年 Hanguang. All rights reserved.
+//
+
+import UIKit
+import DeeplinkNavigator
+
+public extension DeeplinkNavigator {
+    
+    
+    /// Try to pop to a viewController if it's exist in UINavigationController's stack already,
+    /// if not, push a new viewController then.
+    ///
+    /// - parameter url: The URL to find view controllers.
+    /// - parameter from: The navigation controller which is used to push a view controller. Use application's top-most
+    ///     view controller if `nil` is specified. `nil` by default.
+    /// - parameter animated: Whether animates view controller transition or not. `true` by default.
+    ///
+    /// - returns: The pushed view controller. Returns `nil` if there's no matching view controller or failed to push
+    ///            a view controller.
+    @discardableResult
+    public func pushOrPopTo(
+        _ url: DeeplinkConvertible,
+        context: NavigationContext? = nil,
+        from: DeeplinkPushable? = nil,
+        animated: Bool = true
+        ) -> UIViewController? {
+        if popTo(url, context: context, from: from, animated: animated) == nil {
+            return push(url, context: context, from: from, animated: animated)
+        }
+        return nil
+    }
+    
+    /// Try to pop to a viewController if it's exist in UINavigationController's stack already,
+    /// return false if cannot find related viewController.
+    ///
+    /// - parameter url: The URL to find view controllers.
+    /// - parameter from: The navigation controller which is used to push a view controller. Use application's top-most
+    ///     view controller if `nil` is specified. `nil` by default.
+    /// - parameter animated: Whether animates view controller transition or not. `true` by default.
+    ///
+    /// - returns: A Bool value indicated success or failure
+    @discardableResult
+    public func popTo(
+        _ url: DeeplinkConvertible,
+        context: NavigationContext? = nil,
+        from: DeeplinkPushable? = nil,
+        animated: Bool = true
+        ) -> UIViewController?{
+        if let viewController = reloadViewController(url: url, context: context, from: from) {
+            guard let navigationController = from?.lhw_navigationController ?? UIViewController.lhw_topMost?.navigationController else { return nil }
+            navigationController.popToViewController(viewController, animated: true)
+            return viewController
+        }
+        return nil
+    }
+    
+    @discardableResult
+    public func reloadTo(
+        _ url: DeeplinkConvertible,
+        context: NavigationContext? = nil,
+        from: DeeplinkPushable? = nil
+        ) -> UIViewController? {
+        return reloadViewController(url: url, context: context , from: from)
+    }
+    
+    fileprivate func reloadViewController(url: DeeplinkConvertible,
+                                       context: NavigationContext? = nil,
+                                       from: DeeplinkPushable? = nil) -> UIViewController?{
+        if let urlMatchComponents = DeeplinkMatcher.default.match(url, scheme: self.scheme, from: Array(self.urlMap.keys)) {
+            guard let item = self.urlMap[urlMatchComponents.pattern] else { return nil }
+            guard let vcType = item.navigable as? UIViewController.Type else { return nil }
+            guard let navigationController = from?.lhw_navigationController ?? UIViewController.lhw_topMost?.navigationController else { return nil }
+            if let viewController = navigationController.viewControllers.reversed().first(where: { (vc) -> Bool in
+                return vc.classForCoder == vcType
+            }) {
+                if let lifeCycle = viewController as? LifeCycleable{
+                    let navigation = DeeplinkNavigation(
+                        url: url,
+                        values: urlMatchComponents.values,
+                        mappingContext: item.mappingContext,
+                        navigationContext: context
+                    )
+                    lifeCycle.navigationLoad(parameter: navigation.parameter, needRequest: navigation.needRequest)
+                }
+                return viewController
+            }
+        }
+        return nil
+    }
+}
---
+++ DeeplinkNavigator/example/MogoLifeCycle/LifeCycleable.swift
@@ -0,0 +1,111 @@
+//
+//
+//                        _____
+//                       / ___/____  ____  ____ _
+//                       \__ \/ __ \/ __ \/ __ `/
+//                      ___/ / /_/ / / / / /_/ /
+//                     /____/\____/_/ /_/\__, /
+//                                      /____/
+//
+//                .-~~~~~~~~~-._       _.-~~~~~~~~~-.
+//            __.'              ~.   .~              `.__
+//          .'//                  \./                  \\`.
+//        .'//                     |                     \\`.
+//      .'// .-~"""""""~~~~-._     |     _,-~~~~"""""""~-. \\`.
+//    .'//.-"                 `-.  |  .-'                 "-.\\`.
+//  .'//______.============-..   \ | /   ..-============.______\\`.
+//.'______________________________\|/______________________________`.
+//
+//
+//  LifeCycleable.swift
+//  DeeplinkNavigator
+//
+//  Created by song on 2017/7/15.
+//  Copyright © 2017年 Hanguang. All rights reserved.
+//
+
+import UIKit
+import DeeplinkNavigator
+
+//MARK: - Init方法初始化 InitLifeCycleable可以接在ViewController的头上
+
+protocol InitLifeCycleable : LifeCycleable, InitNavigable{
+    
+}
+
+//MARK: - Xib方法初始化 XibLifeCycleable可以接在ViewController的头上 如果使用默认初始化必须保证Identifier为类名
+
+protocol XibLifeCycleable : LifeCycleable, XibNavigable{
+    
+}
+
+//MARK: - Storyboard方法初始化 StoryboardLifeCycleable可以接在ViewController的头上 
+          //如果使用默认初始化必须保证Identifier为类名.而且只能是在Main-Storyboard上
+
+public protocol StoryboardLifeCycleable : LifeCycleable, StoryboardNavigable{
+    
+     static func viewControllerFromStoryBoard(navigation: DeeplinkNavigation) -> UIViewController?
+}
+
+extension InitLifeCycleable  where Self : UIViewController {
+    
+    init?(navigation: DeeplinkNavigation) {
+        self.init()
+        self.navigation = navigation
+    }
+}
+
+extension XibLifeCycleable  where Self : UIViewController {
+    
+    static func viewControllerFromXib(navigation: DeeplinkNavigation) -> UIViewController?{
+        let vc = self.init(nibName: String(describing: self), bundle: nil)
+        vc.navigation = navigation
+        return  vc
+    }
+}
+
+extension StoryboardLifeCycleable  where Self : UIViewController {
+    
+    public static func viewControllerFromStoryBoard(navigation: DeeplinkNavigation) -> UIViewController?{
+
+        if let vc = MGViewControllerManager.ViewControllerInit(viewControllerType: Self.self) {
+            vc.navigation = navigation
+            return vc
+        }
+        else
+        {
+            let sb = UIStoryboard(name: "RenterStoryBoard", bundle: nil)
+            if let vc = sb.instantiateViewController(withIdentifier: String(describing: self)) as? Self{
+                vc.navigation = navigation
+                return  vc
+            }
+        }
+        
+        return nil
+    }
+}
+
+//MARK: - 内部使用加了生命周期 外界不用调用
+
+public protocol LifeCycleable : class {
+    
+    var navigation:DeeplinkNavigation?{ get set }
+    
+    /*! 会在ViewController中调用super.viewDidLoad()时调用 */
+    func navigationLoad(parameter: [String: Any]?,needRequest:Bool)
+}
+
+private var navigationKey: Void?
+
+extension LifeCycleable {
+    
+    public var navigation:DeeplinkNavigation?{
+        get{
+            return objc_getAssociatedObject(self, &navigationKey) as? DeeplinkNavigation
+        }
+        set(newValue){
+            objc_setAssociatedObject(self, &navigationKey, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+}
+
